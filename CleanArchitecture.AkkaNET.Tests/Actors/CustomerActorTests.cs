2ff8625938dab3971e79045ae8cb0b6f59a996de
unknown <brantwills@gmail.com>
2018-12-19 07:48:22 -0500

    merge: pull in from master

---
+++ CleanArchitecture.AkkaNET.Tests/Actors/CustomerActorTests.cs
@@ -0,0 +1,26 @@
+﻿using Akka.Actor;
+using Akka.TestKit.Xunit2;
+using CleanArchitecture.AkkaNET.Commands;
+using CleanArchitecture.AkkaNET.Providers;
+using CleanArchitecture.Domain.Entities;
+using CleanArchitecture.Domain.Interfaces;
+using Moq;
+using Xunit;
+
+namespace CleanArchitecture.AkkaNET.Tests.Actors
+{
+
+    public class CustomerActorTests : TestKit
+    {
+        [Fact]
+        public void ItShouldTellCreateCommand()
+        {
+            var context = new Mock<ICustomerRepository>();
+            var provider = new CustomerActorProvider(Sys, context.Object);
+            context.Setup(_ => _.CreateCustomer(It.IsAny<Customer>()));
+
+            var actor = provider.Get();
+            actor.Tell(new CustomerCommands.CreateCustomer(1, "first", "last"));
+        }
+    }
+}
